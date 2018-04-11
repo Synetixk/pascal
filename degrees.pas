846953c94045daf7f40d0efce369860ec29897bb
Synetixk <36851705+Synetixk@users.noreply.github.com>
2018-04-11 20:43:36 +0500

    degrees
    ds

---
+++ degrees.pas
@@ -0,0 +1,14 @@
+﻿var number,number1,degrees,degrees1,a:integer;
+begin
+writeln('Введите число : ');
+readln(number);
+writeln('Введите степень : ');
+readln(degrees);
+number1:=number;
+degrees1:=degrees;
+degrees:=degrees-1;
+for a:=1 to degrees do begin
+number:=number*number1;
+end;
+writeln('Итог : ',number,' (', number1, ' в ', degrees1, ' степени)');
+end.
